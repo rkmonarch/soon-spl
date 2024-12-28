0ae99c0ce7fc9ffc9d1c854df2b267f25558501b
rkmonarch <rahulkulkarni5234@gmail.com>
2024-12-28 19:32:45 +0530

    feat: program compatible with the metaplex SDK

--- src/index.ts
+++ src/index.ts
@@ -4,119 +4,14 @@ import {
   some,
   createSignerFromKeypair,
   signerIdentity,
-  publicKey,
-  Cluster,
-  Umi,
 } from "@metaplex-foundation/umi";
 import { createFungible } from "@metaplex-foundation/mpl-token-metadata";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
-import { Keypair } from "@solana/web3.js";
+import { Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { base58 } from "@metaplex-foundation/umi/serializers";
 
-// Function to switch to SOON and register required programs
-export async function umiSwitchToSoon(umi: Umi) {
-  // Register Token Metadata Program
-
-  // Devnet program ids
-  const mplTokenMetadata = publicKey(
-    "6C4GR9AtMGF25sjXKtdB7A6NVQUudEQWw97kG61pGuA1"
-  );
-  const mplCandyMachineCore = publicKey(
-    "GFmqavo1M8wEL3a4uouSCaDX5CJapcYWXTcZ4TK6L9ad"
-  );
-  const mplcandyGuard = publicKey(
-    "3g5Pe9ZoDmhA4k1ooFhxgtMWNesTYRdrSAKWMfjr2YxH"
-  );
-
-  // Testnet program ids
-  const mplCandyMachineCoreTestnet = publicKey(
-    "2g5gta4shjCtCmMA6mzJ2wbj2P9QtPQuY27gK7z3pv5j"
-  );
-
-  const mplCandyGuardTestnet = publicKey(
-    "FFwcwSGPurGpQsu41wXFVZpgmWJgoZbpqcLF84eKNeM1"
-  );
-
-  const mplTokenMetadataTestnet = publicKey(
-    "6C4GR9AtMGF25sjXKtdB7A6NVQUudEQWw97kG61pGuA1"
-  );
-
-  umi.programs.add(
-    {
-      name: "mplTokenMetadata",
-      publicKey: mplTokenMetadataTestnet,
-      getErrorFromCode: (code: number, cause?: Error) => null,
-      getErrorFromName: (name: string, cause?: Error) => null,
-      isOnCluster: (cluster: Cluster) => true,
-    },
-    true
-  );
-
-  // Register Candy Machine Core Program
-  umi.programs.add(
-    {
-      name: "mplCandyMachineCore",
-      publicKey: mplCandyMachineCoreTestnet,
-      getErrorFromCode: (code: number, cause?: Error) => null,
-      getErrorFromName: (name: string, cause?: Error) => null,
-      isOnCluster: (cluster: Cluster) => true,
-    },
-    true
-  );
-
-  // Register Candy Machine Program
-  umi.programs.add(
-    {
-      name: "mplCandyMachine",
-      publicKey: mplCandyMachineCoreTestnet,
-      getErrorFromCode: (code: number, cause?: Error) => null,
-      getErrorFromName: (name: string, cause?: Error) => null,
-      isOnCluster: (cluster: Cluster) => true,
-    },
-    true
-  );
-
-  (umi.programs as any).add(
-    {
-      name: "mplCandyGuard",
-      publicKey: mplCandyGuardTestnet,
-      getErrorFromCode: (code: number, cause?: Error) => null,
-      getErrorFromName: (name: string, cause?: Error) => null,
-      isOnCluster: (cluster: Cluster) => true,
-      // ignore errors
-      availableGuards: [
-        "botTax",
-        "solPayment",
-        "tokenPayment",
-        "startDate",
-        "thirdPartySigner",
-        "tokenGate",
-        "gatekeeper",
-        "endDate",
-        "allowList",
-        "mintLimit",
-        "nftPayment",
-        "redeemedAmount",
-        "addressGate",
-        "nftGate",
-        "nftBurn",
-        "tokenBurn",
-        "freezeSolPayment",
-        "freezeTokenPayment",
-        "programGate",
-        "allocation",
-        "token2022Payment",
-      ],
-    },
-    true
-  );
-}
-// Set up the Umi instance
 //change this to devnet/testnet accordingly
-const umi = createUmi("https://rpc.testnet.soo.network/rpc").use(
-  mplTokenMetadata()
-);
+const umi = createUmi("https://rpc.testnet.soo.network/rpc");
 
 // Payer secret key for signing transactions
 const payerSecretKey = new Uint8Array([
@@ -128,6 +23,12 @@ const payerSecretKey = new Uint8Array([
 
 // Create keypair and signer
 let keypair = umi.eddsa.createKeypairFromSecretKey(payerSecretKey);
+const balance = await umi.rpc.getBalance(keypair.publicKey);
+console.log(
+  "Payer Balance:",
+  (Number(balance.basisPoints) / LAMPORTS_PER_SOL).toFixed(2),
+  "SOL"
+);
 const payer = Keypair.fromSecretKey(payerSecretKey);
 console.log("Payer Public Key:", payer.publicKey.toBase58());
 
@@ -139,9 +40,6 @@ const mint = generateSigner(umi);
 
 // Main function to create a fungible token
 async function main() {
-  // Switch Umi instance to SOON Devnet/Testnet by adding necessary programs
-  await umiSwitchToSoon(umi);
-
   // Create the fungible token (e.g., BONK)
   const txResponse = await createFungible(umi, {
     mint,
